d47b90c2de87a8d3fb51da3818fa78b40c7d3974
Kiran-Ambatt <kiranat.487@gmail.com>
2022-05-02 10:39:17 +0530

    my commit

---
+++ c# 3/Arrayhandler/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHandler
+{
+    class myArray
+    {
+        static readonly int MAX = 5;
+        public int[] stack = new int[MAX];
+        public int top = 0;
+
+        public void push(int value)
+        {
+            if (top < MAX)
+            {
+                this.stack[top] = value;
+                top++;
+            }
+            //ex
+        }
+        public void pop()
+        {
+            if (top < 0)
+            {
+                return;
+            }
+            //Console.WriteLine(this.stack[top]);
+            top--;
+           
+        }
+
+        public void print()
+        {
+            for(int i = 0; i<top; i++)
+            {
+                Console.WriteLine(stack[i]);
+            }
+        }
+    }
+}
