81cc1f86d8e94731024cdbc19b83a9b7f1e708cb
Kiran-Ambatt <kiranat.487@gmail.com>
2022-05-02 11:15:25 +0530

    Commit for array handling assignments

---
+++ c# 3/Arrayhandler/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayHandler;
+
+Console.WriteLine("Welcome to Array Hanlder!");
+
+myArray myArrayObj = new myArray();
+myArrayObj.push(50);
+myArrayObj.push(60);
+myArrayObj.push(70);
+//myArrayObj.print();
+myArrayObj.pop();
+myArrayObj.print();
+
+//myArrayObj.print();
+/*myArrayObj.push(50);
+myArrayObj.push(60);
+myArrayObj.push(98);
+myArrayObj.push(77);
+myArrayObj.print();
+
+myArrayObj.print();
+
+/*
+myArrayObj.push(35);
+
+myArrayObj.push(45);
+myArrayObj.print();
+myArrayObj.pop();*/
