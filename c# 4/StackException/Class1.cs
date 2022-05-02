bb243eebb6c671527d01b4a537f7e6b81fcc207f
Kiran-Ambatt <kiranat.487@gmail.com>
2022-05-02 11:26:47 +0530

    Assignment 4

---
+++ c# 4/StackException/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayHandler
+{
+    public class StackFullException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Stack is full";
+            }
+        }
+   
+
+    }
+    public class StackEmptyException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Stack is Empty";
+            }
+        }
+
+
+    }
+    class myArray
+    {
+        static readonly int MAX = 5;
+        public int[] stack = new int[MAX];
+        public int top = 0;
+
+        public void push(int value)
+        {
+            if (top < MAX)
+            {
+                this.stack[top] = value;
+                top++;
+            }
+            else
+            {
+                throw new StackFullException();
+            }
+            
+        }
+        public void pop()
+        {
+            if (top == 0)
+            {
+                throw new StackEmptyException();
+            }
+            else
+            {
+                top--;
+            }
+            
+        }
+
+        public void print()
+        {
+            for(int i = 0; i<top; i++)
+            {
+                Console.WriteLine("stack is:",stack[i]);
+            }
+        }
+    }
+}
---
+++ c# 4/StackException/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayHandler;
+
+Console.WriteLine("Welcome to Array Hanlder!");
+
+myArray myArrayObj = new myArray();
+
+
+try
+{
+    myArrayObj.print();
+    myArrayObj.push(10);
+    myArrayObj.push(20);
+    myArrayObj.push(30);
+    myArrayObj.push(40);
+    myArrayObj.push(50);
+
+    // except exception
+    myArrayObj.push(60);
+
+    myArrayObj.print();
+
+}
+catch(StackFullException error)
+{
+    Console.WriteLine (error.Message);
+}
+
+try
+{
+    myArrayObj.pop();
+    myArrayObj.pop();
+    myArrayObj.pop();
+    myArrayObj.pop();
+    myArrayObj.pop();
+
+    // expect exception
+    myArrayObj.pop();
+
+    myArrayObj.print();
+}
+catch (StackEmptyException error)
+{
+    Console.WriteLine(error.Message);
+}
+
+
+
+
